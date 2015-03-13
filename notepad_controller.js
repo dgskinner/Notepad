b31b631f6ddef361298141809dd8fc65094d7873
Duncan Skinner <skinnerduncan@gmail.com>
2015-03-12 22:48:15 -0700

    avoid error on attempt to save duplicate note

--- notepad_controller.js
+++ notepad_controller.js
@@ -2,17 +2,28 @@ app.controller("notepadController", function($scope) {
 	$scope.noteText = "";
 	$scope.charLeft = function () {
 		return (250 - $scope.noteText.length);
-	};
+	}
 	$scope.savedNotes = [];
 	$scope.saveNote = function () {
-		if ($scope.charLeft() >= 0 ) {
+		var duplicate = false;
+		$scope.savedNotes.forEach( function (note) {
+			if (note === $scope.noteText) {
+				alert ("an identical note has already been saved.");
+				duplicate = true;
+			}
+		});
+		if (duplicate) {
+			return;
+		}
+		
+		if ($scope.charLeft() < 0 ) {
+			alert("Your note could not be saved because it is too long.");
+		} else {
 			$scope.savedNotes.push($scope.noteText);
 			alert("Your note was saved.");
-		} else {
-			alert("Your note could not be saved because it is too long.");
 		}
-	};
+	}
 	$scope.clearNote = function () {
 		$scope.noteText = "";
-	};
+	}
 });
