b92c2e3c8e2a8c7eaa2214a3ae4d7b6e7f459bad
Duncan Skinner <skinnerduncan@gmail.com>
2015-03-12 20:27:50 -0700

    Clear button clears textarea

--- notepad_controller.js
+++ notepad_controller.js
@@ -7,10 +7,12 @@ app.controller("notepadController", function($scope) {
 	$scope.saveNote = function () {
 		if ($scope.charLeft() >= 0 ) {
 			$scope.savedNotes.push($scope.noteText);
-			console.log($scope.savedNotes);
 			alert("Your note was saved.");
 		} else {
 			alert("Your note could not be saved because it is too long.");
 		}
+	};
+	$scope.clearNote = function () {
+		$scope.noteText = "";
 	};
 });
